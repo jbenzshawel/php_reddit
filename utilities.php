58892362e41fd9c3a370329b4617dfba84721939
jbenzshawel <john-benzshawel@uiowa.edu>
2014-02-28 18:52:46 -0600

    Fixed an out of place method

--- utilities.php
+++ utilities.php
@@ -169,7 +169,7 @@ class users {
         $res = $this->make_query($query);
         return $res['username'];
     }
-
+    // OLD WAY in process of phasing out. Use make_prepared_query in the future
     private function make_query($query){
 		if ( $result = $this->db->query($query) ) {
 			while ($row = $result->fetch_assoc()) {
@@ -180,42 +180,77 @@ class users {
 		}
 	}
 
-    // comment age
-    public function age($commentid){
-        $this->commentid = intval($commentid);
-        // Declare prepared query parameters
-        $select_set = 'date_comment';
-        $column = array('date_comment');
-        $table = 'comments';
-        $whereArgs = 'commentid = ?';
-        $argTypes = array('i');
-        $argVars = array($this->commentid);
-        // Get date created
-        $date_created = $this->make_prepared_query($select_set, $column, $table, $whereArgs, $argTypes, $argVars);
-        // Get today's date
-        $todays_date = $this->today->format('Y-m-d H:i:s');//getTimestamp();
-        // Convert dates to timestamps
-        $res[0] = strtotime($todays_date);
-        $res[1] = strtotime($date_created[0]['date_comment']);
-        // Get change in time
-        $etime = $res[0] - $res[1];
-        // If time less than 1 second return 0 seconds
-        if ($etime < 1){
-            return '0 seconds';
-        }
-        // Prepare array for comparing/calculating time in different units
-        $time_units = array( 12 * 30 * 24 * 60 * 60  =>  'year', 30 * 24 * 60 * 60 => 'month',
-            24 * 60 * 60 => 'day', 60 * 60  => 'hour', 60 => 'minute', 1 =>  'second' );
-        foreach ($time_units as $secs => $str){
-            $time_value = $etime / $secs;
-            if ($time_value <= 48){
-                $rounded_time = round($time_value, 2);
-                $res[2] =  $rounded_time . ' ' . $str . ($rounded_time > 1 ? 's' : '') . ' ago';
+    /**
+     * Private method that makes a prepared query. Parameters defined below:
+     * @param $select_set = string of column names/values to be selected. For example "`username`, `userid`,"
+     * @param $column = array of column names in $select_set
+     * @param $table = string name of table or tables to be selected from
+     * @param $whereArgs = string value of where arguments if needed. can be empty string
+     * @param $argTypes = array with each letter i, s, f, etc of variable type for arguments as a string
+     * @param $argVars = array of variables for whereArgs.
+     *  Returns results ($query_result) in array of arrays with "column_name" => "value"
+     */
+    private function make_prepared_query($select_set, $column, $table, $whereArgs, $argTypes, $argVars ){
+        // Make sure all parameters are proper type
+        $select_set = (is_string($select_set)) ? $select_set : false;
+        $column = (is_array($column)) ? $column : false;
+        $table = (is_string($table)) ? $table : false;
+        $whereArgs = (is_string($whereArgs)) ? $whereArgs : ($whereArgs == '' ) ? $whereArgs : false;
+        $argTypes = (is_array($argTypes)) ? $argTypes : false;
+        $argVars = (is_array($argVars)) ? $argVars : false;
+        // If parameter format wrong return error
+        if(!$select_set or !$column or !$table or !$whereArgs or !$argTypes or !$argVars){
+            return "Error: Improper parameter formats see method declaration for parameter information!";
+        } else {
+            // Proper procedure query order: prepare -> bind_param -> execute -> store_result -> bind_result -> fetch
+            // prepare
+            $stmt = $this->db->prepare("SELECT $select_set FROM $table WHERE $whereArgs") ;
+            // bind parameters by reference
+            $params = array_merge($argTypes, $argVars);
+            call_user_func_array(array(&$stmt, 'bind_param'), $this->refValues($params));
+            // execute
+            $stmt->execute();
+            // store results
+            $stmt->store_result();
+            // bind results (similar to bind parameter)
+            $data = array() ; // Array that accepts the data.
+            $params = array() ; // Parameter array passed to 'bind_result()'
+            foreach($column as $col_name){
+                // 'fetch()' will assign fetched value to the variable '$data[$col_name]'
+                $params[] =& $data[$col_name] ;
             }
+            $res = call_user_func_array(array($stmt, "bind_result"), $params) ;
+            // if success fetch results
+            if(! $res){
+                $query_result =  "bind_result() failed: " . $this->db->error . "\n" ;
+            } else {
+                $i=0;
+                // fetch all rows of result and store in $query_result
+                while($stmt->fetch()){
+                    $query_result[$i] = array();
+                    foreach($data as $key=>$value)
+                        $query_result[$i][$key] = $value;
+                    $i++;
+                }
+            }
+            // close statement and db connection
+            $stmt->close() ;
+            $this->db->close() ;
+            // return query results
+            return $query_result;
         }
-        // Return comment age as string
-        return $res[2];
     }
+    // Returns inputted array by reference
+    private function refValues($arr){
+        $refs = array();
+        foreach ($arr as $key => $value){
+            $refs[$key] = &$arr[$key];
+        }
+        return $refs;
+    }
+
+
+
 } # END CLASS
 
 // class for everything post related (creating, viewing, editing)
